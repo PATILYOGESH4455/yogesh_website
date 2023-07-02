c07a84208925b8929a3e356f6ed3efbffd0ee886
Yogesh D Patil <60956756+PATILYOGESH4455@users.noreply.github.com>
2023-07-02 14:43:28 +0530

    added allowed host

--- yogesh_website/settings.py
+++ yogesh_website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+qf#j*d^dk2yegw%hb&9qoq-5tx+ev)j6k*l90ykw9&uh@5p*_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.86.94.5']
 
 
 # Application definition
